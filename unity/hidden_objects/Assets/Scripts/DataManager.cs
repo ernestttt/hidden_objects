2f763ac8b8d3d8a235b314483f0f0d1bbcdd98b7
Errnest <yuri.golubovski091@gmail.com>
2024-06-06 18:03:38 +0300

    image loading

--- unity/hidden_objects/Assets/Scripts/DataManager.cs
+++ unity/hidden_objects/Assets/Scripts/DataManager.cs
@@ -20,5 +20,28 @@ public class DataManager{
         UnityWebRequest request = UnityWebRequest.Get(_config.ContentPath);
         await request.SendWebRequest();
         LevelsResult result = JsonUtility.FromJson<LevelsResult>(request.downloadHandler.text);
+        LevelData[] levelDatas = result.levels;
+
+        levels = new Dictionary<int, LevelData>();
+        for (int i = 0; i < levelDatas.Length; i++)
+        {
+            LevelData levelData = levelDatas[i];
+            levels.Add(levelData.id, levelData);
+        }
+    }
+
+    private async UniTask CheckImagesLoading(){
+        List<Texture2D> textures = new List<Texture2D>();
+        foreach (LevelData data in levels.Values){
+            Texture2D texture = await LoadTexture(data.imageUrl);
+            textures.Add(texture);
+        }
+    }
+
+    public async UniTask<Texture2D> LoadTexture(string url){
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        await request.SendWebRequest();
+        Texture2D texture =  DownloadHandlerTexture.GetContent(request);
+        return texture;
     }
 }
